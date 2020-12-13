50cb0bfbdb21de87c021b509a60cd1860a22070f
Zapta <zapta@users.noreply.github.com>
2020-12-12 19:48:04 -0800

    Windows automesh script now works.

---
+++ duet3d_automesh/duet3d_automesh.bat
@@ -0,0 +1,8 @@
+
+; An adapter to add single quote to the file path before involing the bash script.
+
+
+"c:\Program Files\Git\bin\bash.exe" -c "/c/projects/util/repo/duet3d_automesh/duet3d_automesh_win.sh '%1'"
+
+
+
--- duet3d_automesh/duet3d_automesh_win.sh
+++ duet3d_automesh/duet3d_automesh_win.sh
@@ -6,9 +6,14 @@ python3="/c/Users/User/AppData/Local/Programs/Python/Python38-32/python"
 
 date > $log
 echo "Args: $*" >> $log
+echo "Arg1: $1" >> $log
+echo "Arg2: $2" >> $log
 echo >> $log
 
-$python3 /c/projects/util/repo/duet3d_automesh/duet3d_automesh.py "$*" 2>&1 > $log
+outfile=$(cygpath -u "$1")
+
+$python3 /c/projects/util/repo/duet3d_automesh/duet3d_automesh.py "$outfile" 2>&1 > $log
+
 
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
@@ -16,7 +21,8 @@ if [ $RESULT -eq 0 ]; then
   #osascript -e 'display notification "OK." with title "Duet automesh"'
 else
   #osascript -e 'display notification "Failed." with title "Duet automesh"'
-  msg user "Auto-mesh error"
+  msg user "Auto-mesh error. See $log"
+  pause 10
 fi
 
 
