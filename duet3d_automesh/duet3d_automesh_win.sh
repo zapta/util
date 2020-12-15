cb4049e843b008e270ced88ef05f21c3b256aef2
Zapta <zapta@users.noreply.github.com>
2020-12-15 08:08:17 -0800

    Updated win automesh shell script.

--- duet3d_automesh/duet3d_automesh_win.sh
+++ duet3d_automesh/duet3d_automesh_win.sh
@@ -1,10 +1,49 @@
 #!/bin/bash -x
 
+# Ascii banners from:
+# http://www.patorjk.com/software/taag/#p=display&h=0&f=Banner3&t=error
+
 log="/tmp/automesh.txt"
-#python3="/Library/Frameworks/Python.framework/Versions/3.6/bin/python3"
 python3="/c/Users/User/AppData/Local/Programs/Python/Python38-32/python"
 
 date > $log
+
+
+function print_ok() {
+  echo
+  echo
+  echo
+  echo "  #######  ##    ## "
+  echo " ##     ## ##   ##  "
+  echo " ##     ## ##  ##   "
+  echo " ##     ## #####    "
+  echo " ##     ## ##  ##   "
+  echo " ##     ## ##   ##  "
+  echo " #######  ##    ##  "
+  echo
+  sleep 3
+}
+
+
+
+function print_error() {
+  echo
+  echo
+  echo
+  echo " ######## ########  ########   #######  ########  "
+  echo " ##       ##     ## ##     ## ##     ## ##     ## "
+  echo " ##       ##     ## ##     ## ##     ## ##     ## "
+  echo " ######   ########  ########  ##     ## ########  "
+  echo " ##       ##   ##   ##   ##   ##     ## ##   ##   "
+  echo " ##       ##    ##  ##    ##  ##     ## ##    ##  "
+  echo " ######## ##     ## ##     ##  #######  ##     ## "
+  echo
+  sleep 60
+}
+
+
+
+
 echo "Args: $*" >> $log
 echo "Arg1: $1" >> $log
 echo "Arg2: $2" >> $log
@@ -12,17 +51,20 @@ echo >> $log
 
 outfile=$(cygpath -u "$1")
 
-$python3 /c/projects/util/repo/duet3d_automesh/duet3d_automesh.py "$outfile" 2>&1 > $log
+
+$python3 /c/projects/util/repo/duet3d_automesh/duet3d_automesh.py "$outfile" 2>&1 >> $log
 
 
 RESULT=$?
 if [ $RESULT -eq 0 ]; then
-  msg user "Auto-mesh completed ok"
-  #osascript -e 'display notification "OK." with title "Duet automesh"'
+  echo
+  cat $log
+  (set +x ; print_ok )
 else
-  #osascript -e 'display notification "Failed." with title "Duet automesh"'
-  msg user "Auto-mesh error. See $log"
-  pause 10
+  echo
+  cat $log
+  (set +x; print_error)
+  #msg user "Auto-mesh error. See $log"
 fi
 
 
