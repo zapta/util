5f3c85dab398db3c5808c883e18fe45a7e78c9fd
Zapta <zapta@users.noreply.github.com>
2021-05-31 09:35:23 -0700

    Flushing pending changes.

--- duet3d_automesh/duet3d_automesh.bat
+++ duet3d_automesh/duet3d_automesh.bat
@@ -1,7 +1,7 @@
 ; An adapter to add single quote to the file path before involing the bash script.
 
 
-"c:\Program Files\Git\bin\bash.exe" -c '/c/projects/util/repo/duet3d_automesh/duet3d_automesh_win.sh %*'
+"c:\Program Files\Git\bin\bash.exe" -c '/c/projects/util/repo/duet3d_automesh/duet3d_automesh_win.sh \'%*\''
 
 if NOT ["%errorlevel%"]==["0"] (
     pause
